148d20c8af6ce11ccd0fad1a17ac12a1977af1ae
amiirhussain <amirh.pcr@gmail.com>
2023-11-26 16:54:44 +0500

    update user profile

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,16 +4,22 @@ import CryptoJS from 'crypto-js';
 // Update User
 export const updateUser = async (req, res, next) => {
   try {
+    const userId = req.params.id; // Extract the user ID from the request parameters
+
     const updatedUser = await User.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      },
+      userId,
+      { $set: req.body },
       { new: true },
     );
-    res.status(200).send(updatedUser);
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json(updatedUser);
   } catch (error) {
-    next(error);
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 };
 
